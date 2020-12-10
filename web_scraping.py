14dac3b6d832740da5b13d5a5e33c874d76b994a
Elbert Ang <74346365+zuspitez@users.noreply.github.com>
2020-12-10 13:12:08 +0800

    Add files via upload

---
+++ web_scraping.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+# import pandas as pd
+
+page = requests.get("http://forecast.weather.gov/MapClick.php?lat=37.7772&lon=-122.4168")
+soup = BeautifulSoup(page.content, 'html.parser')
+seven_day = soup.find(id="seven-day-forecast")
+forecast_items = seven_day.find_all(class_="tombstone-container")
+overnight = forecast_items[0]
+print(overnight.prettify())
