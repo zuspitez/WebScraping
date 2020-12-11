700dc895e9a7133848d8f1d105eeec57db348ce3
Elbert Ang <74346365+zuspitez@users.noreply.github.com>
2020-12-11 09:42:36 +0800

    Add files via upload

---
+++ web_scraping #2.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+#import pandas as pd
+
+page = requests.get("http://forecast.weather.gov/MapClick.php?lat=37.7772&lon=-122.4168")
+soup = BeautifulSoup(page.content, 'html.parser')
+
+seven_day = soup.find(id="seven-day-forecast")
+
+period_tags = seven_day.select(".tombstone-container .period-name")
+print(period_tags)
+
+periods = [pt.get_text() for pt in period_tags]
+print(periods)
+
+short_descs = [sd.get_text() for sd in seven_day.select(".tombstone-container .short-desc")]
+temps = [t.get_text() for t in seven_day.select(".tombstone-container .temp")]
+descs = [d["title"] for d in seven_day.select(".tombstone-container img")]
+print(short_descs)
+print(temps)
+print(descs)
